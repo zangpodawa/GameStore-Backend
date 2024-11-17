bf84d751d8439c0ed9156f451acc47955df8c4bc
Dawa <dzangpo29@gmail.com>
2024-11-17 15:16:43 +1000

    FIrst commit

---
+++ GameStore.Api/Data/DataExtension.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Data;
+
+public static class DataExtension
+{
+  public static async Task MigrateDbAsync(this WebApplication app)
+  {
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<GameStoreContext>();
+    await dbContext.Database.MigrateAsync();
+  }
+}
